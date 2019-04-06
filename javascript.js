8fac0fe897d6f59ad0a87eeb189da266b52b9892
OrionHolcomb <orionholcomb@gmail.com>
2019-04-06 12:13:40 -0400

    merged code.js and javascript.js

--- javascript.js
+++ javascript.js
@@ -27,3 +27,30 @@ $('#submitButton').on('click', function(){
     $('#employeeDate').text('');
 })
 setInterval();
+
+// Adding the firebase config
+var config = {
+    apiKey: "AIzaSyBbV6J7yzzpFINlBlmMvMZBFJo7YwqDxEg",
+    authDomain: "timesheetproject-c7cf3.firebaseapp.com",
+    databaseURL: "https://timesheetproject-c7cf3.firebaseio.com",
+    projectId: "timesheetproject-c7cf3",
+    storageBucket: "timesheetproject-c7cf3.appspot.com",
+    messagingSenderId: "552631608999"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  database.ref().push({
+      Name: employeeName,
+      Role: employeeRole,
+      Salary: employeeSalary,
+      Date: employeeDate
+  });
+
+  database.ref().on('child_added', function(snapshot) {
+      nameCell = database.Name;
+      roleCell = database.Role;
+      salary = database.Salary;
+      startDate = database.Date;
+  });
